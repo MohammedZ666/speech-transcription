6e757db7865c1a5b6f843076c66f92a216cdeef0
Abu Obaida Zishan <abuobaidazishan@gmail.com>
2023-09-11 17:28:43 +0600

    added bundled client to server resource folder

---
+++ src/main/resources/static/precache-manifest.dc095651ed69456943341966dfe1fc0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b7739f34ef2c1b142a9",
+    "url": "/static/css/main.0668a75d.chunk.css"
+  },
+  {
+    "revision": "3b7739f34ef2c1b142a9",
+    "url": "/static/js/main.3b7739f3.chunk.js"
+  },
+  {
+    "revision": "cac9bec7ac4267c58495",
+    "url": "/static/js/1.cac9bec7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3509d9b0866c8c7fb41b7130e65b420",
+    "url": "/index.html"
+  }
+];
