fe69e14dd9356b53061c8edacb9576dcd6d14cf9
Abu Obaida Zishan <abuobaidazishan@gmail.com>
2023-10-02 21:49:37 +0600

    Clearing processing directory on server start

--- src/main/java/com/ztech/subtly/SubtlyApplication.java
+++ src/main/java/com/ztech/subtly/SubtlyApplication.java
@@ -1,10 +1,13 @@
 package com.ztech.subtly;
 
+import java.io.File;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.apache.commons.io.FileUtils;
 
 @SpringBootApplication
 public class SubtlyApplication {
@@ -21,6 +24,12 @@ public class SubtlyApplication {
 				registry.addMapping("/**").allowedHeaders("*").allowedMethods("*");
 			}
 		};
+	}
+
+	@Bean
+	public void cleanProcessingDirectory() throws IOException {
+		File processing_path = new File(System.getProperty("user.dir"), "processing");
+		FileUtils.cleanDirectory(processing_path);
 	}
 
 }
